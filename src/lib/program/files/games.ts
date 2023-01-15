e915badca989d55bde07fb6e4b44d476b9f5b5b7
Tomás Rinaldi <tomi901@users.noreply.github.com>
2023-01-15 18:26:47 -0300

    Added floppa game icon

--- src/lib/program/files/games.ts
+++ src/lib/program/files/games.ts
@@ -1,20 +1,18 @@
 import type { ExecutableFile } from "../File";
 
 export const files: ExecutableFile[] = [
-    /*
     {
         id: "games/floppa_adventure.exe",
         name: "Floppa Adventure.exe",
-        icon: "/Icons/directory.png",
+        icon: "/Icons/Games/floppa_adventure.png",
         onOpen(system) {
-            system.openWindow("https://v6p9d9t4.ssl.hwcdn.net/html/7156938/floppa_adventure/index.html", {
+            system.openWindow("https://v6p9d9t4.ssl.hwcdn.net/html/7156942/floppa_adventure/index.html", {
                 title: "Floppa Adventure",
                 width: 800,
                 height: 600,
             });
         },
     },
-    */
 ];
 
 export default files;
