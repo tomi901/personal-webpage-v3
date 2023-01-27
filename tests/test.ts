290b0c1ce56f8a76c3bc481ad3da0538d3298959
Tomás Rinaldi <tomi901@users.noreply.github.com>
2023-01-27 12:13:13 -0300

    Added window tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,21 +1,74 @@
-import { expect, test } from '@playwright/test';
+import {expect, test, type Page } from '@playwright/test';
 
-test('Has main element', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('main')).toBeVisible();
+test.describe("Navigation basic tests", () => {
+	test('Has main element', async ({ page }) => {
+		await page.goto('/');
+		await expect(page.getByRole('main')).toBeVisible();
+	});
+
+	test('Has files', async ({ page }) => {
+		await page.goto('/');
+		await expect(page.locator(".files-container")).not.toBeEmpty();
+	});
+
+	test('Invalid path returns 404', async ({ page }) => {
+		await page.goto('/test-this-page-should-definitively-not-exist');
+		await expect(page.getByText(/Error 404/)).toBeVisible();
+	});
+
+	test("/tomas_rinaldi.html page shouldn't return 404", async ({ page }) => {
+		await page.goto('/tomas_rinaldi.html');
+		await expect(page.getByText(/Error 404/)).not.toBeVisible();
+	});
 });
 
-test('Has files', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.locator(".files-container")).not.toBeEmpty();
-});
+test.describe("Window tests", async () => {
+	
+	async function startWithDefaultWindow(page: Page) {
+		await page.goto("/");
+		return await openWindowWithDefaultFile(page);
+	}
+	
+	async function openWindowWithDefaultFile(page: Page) {
+		const file = page.locator(".file").first();
+		await file.click();
+		return page.locator(".window");
+	}
 
-test('Invalid path returns 404', async ({ page }) => {
-	await page.goto('/test-this-page-should-definitively-not-exist');
-	await expect(page.getByText(/Error 404/)).toBeVisible();
-});
+	test("Can open window when clicking a file", async ({ page }) => {
+		await page.goto("/");
+		await expect(page.locator(".window")).not.toBeVisible();
 
-test("/tomas_rinaldi.html page shouldn't return 404", async ({ page }) => {
-	await page.goto('/tomas_rinaldi.html');
-	await expect(page.getByText(/Error 404/)).not.toBeVisible();
+		await openWindowWithDefaultFile(page);
+
+		await expect(page.locator(".window")).toBeVisible();
+	});
+
+	test("Can close window", async ({ page }) => {
+		const window = await startWithDefaultWindow(page);
+
+		await expect(window).toBeVisible();
+
+		const closeButton = window.locator("button.close-btn");
+		await expect(closeButton).toBeVisible();
+		await closeButton.click();
+
+		await expect(window).not.toBeVisible();
+	});
+
+	test("Maximize toggle works correctly", async ({ page }) => {
+		const window = await startWithDefaultWindow(page);
+		const maximizedClass = /maximized/;
+		
+		await expect(window).not.toHaveClass(maximizedClass);
+
+		const maximizeButton = window.locator("button.maximize-btn");
+		await expect(maximizeButton).toBeVisible();
+		
+		await maximizeButton.click();
+		await expect(window).toHaveClass(maximizedClass);
+		
+		await maximizeButton.click();
+		await expect(window).not.toHaveClass(maximizedClass);
+	});
 });
